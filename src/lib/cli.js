cbb1ffb2abf808dbf89ce6afa0b02801518c69cd
Shalev Rahamim <shalevr1997@gmail.com>
2020-03-02 18:43:43 +0200

    Create CLI utility to check github operational status

--- src/lib/cli.js
+++ src/lib/cli.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const axios = require('axios').default;
+const services = require('./services');
+var argv = require('minimist')(process.argv.slice(2));
+
+if(argv._.length !== 1)
+    throw new Error('Some message');
+
+async function getStatus(service)
+{
+    const regex = new RegExp('<.*"' + service.atrName + '".*>(.*)', 'gm');
+    let response = await axios.get(service.url);
+    console.log(regex.exec(response.data) !== null);
+}
+
+for(let service of services)
+{
+    if(service.name == argv._[0])
+    {
+        getStatus(service);
+        break;
+    }
+}
---
+++ src/lib/services.js
@@ -0,0 +1,13 @@
+module.exports = 
+[
+    {
+        name: "github",
+        url: 'https://www.githubstatus.com/',
+        atrName: "status font-large"
+    },
+    {
+        name: "gcp",
+        url: 'https://status.cloud.google.com',
+        atrName: "status"
+    }
+]
