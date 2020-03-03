8cd3f5b506c07add21a5ad65e2d288cb21cb4275
Zohar Yzgeav <43813108+zoharyzgiaev@users.noreply.github.com>
2020-03-03 22:34:02 +0200

    Create CLI utility to check github operational status (#7)
    * Create CLI utility to check github operational status
    
    * General: Semantic changes & adding tests
    
    Co-authored-by: Shalev Rahamim <shalevr1997@gmail.com>

--- src/lib/cli.js
+++ src/lib/cli.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const axios = require("axios").default;
+const services = require("./services");
+var argv = require("minimist")(process.argv.slice(2));
+
+if (argv._.length !== 1)
+  throw new Error(`Livy accepts service name as an argument.`);
+
+const isServiceUp = async service => {
+  const regex = new RegExp('<.*"' + service.attributeName + '".*>(.*)', "gm");
+  let response;
+  response = await axios.get(service.url);
+
+  return regex.exec(response.data) !== null;
+};
+
+if (services[argv._[0]]) {
+  isServiceUp(services[argv._[0]]).then(status => {
+    console.log(status);
+  });
+} else {
+  console.error(`Service name doesn't exists.`);
+}
---
+++ src/lib/tests/cli.test.js
@@ -0,0 +1,16 @@
+describe("CLI test suite", () => {
+  describe("arguments tests", () => {
+    test("argument passed not in services list, should resolve in error", () => {
+      process.argv[2] = "test";
+      const cli = require("../cli");
+    });
+
+    test("no arguments passed should resolve in exception", () => {
+      try {
+        const cli = require("../cli");
+      } catch (e) {
+        expect(e.message).toEqual(`Livy accepts service name as an argument.`);
+      }
+    });
+  });
+});
