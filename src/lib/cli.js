f36becc0c17d319b3584b33e7ac3b3c9eb452d10
zoharyzgiaev <zohar.yzgaev@healthy.io>
2020-03-11 22:42:48 +0200

    Refactor: Prettier changes

--- src/lib/cli.js
+++ src/lib/cli.js
@@ -1,17 +1,24 @@
-const axios = require("axios").default;
-const services = require("./services");
-var argv = require("minimist")(process.argv.slice(2));
+const axios = require('axios').default;
+const services = require('./services');
+var argv = require('minimist')(process.argv.slice(2));
 
-if (argv._.length !== 1)
-  throw new Error(`Livy accepts service name as an argument.`);
+if (argv._.length !== 1) throw new Error(`Livy accepts service name as an argument.`);
 
 const isServiceUp = async service => {
-  const regex = new RegExp('<.*"' + service.attributeName + '".*>(.*)', "gm");
+  const regex = new RegExp('<.*"' + service.attributeName + '".*>(.*)', 'gm');
   let response;
   response = await axios.get(service.url);
 
   return regex.exec(response.data) !== null;
 };
+
+const sleep = new Promise((resolve, reject) => {
+  setTimeout(1000, console.log('hello'));
+});
+const collectioon = ['a', 'b', 'c', 'd'];
+for (const item of collectioon) {
+  await sleep(30000);
+}
 
 if (services[argv._[0]]) {
   isServiceUp(services[argv._[0]]).then(status => {
--- src/lib/tests/cli.test.js
+++ src/lib/tests/cli.test.js
@@ -1,13 +1,13 @@
-describe("CLI test suite", () => {
-  describe("arguments tests", () => {
-    test("argument passed not in services list, should resolve in error", () => {
-      process.argv[2] = "test";
-      const cli = require("../cli");
+describe('CLI test suite', () => {
+  describe('arguments tests', () => {
+    test('argument passed not in services list, should resolve in error', () => {
+      process.argv[2] = 'test';
+      const cli = require('../cli');
     });
 
-    test("no arguments passed should resolve in exception", () => {
+    test('no arguments passed should resolve in exception', () => {
       try {
-        const cli = require("../cli");
+        const cli = require('../cli');
       } catch (e) {
         expect(e.message).toEqual(`Livy accepts service name as an argument.`);
       }
